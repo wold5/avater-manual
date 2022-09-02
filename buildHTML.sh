5fcacbc65ac901b7dad0550b110277e742c66e37
William Ouwehand <mail@syncoda.nl>
2022-09-02 16:19:11 +0200

    output to build

--- buildHTML.sh
+++ buildHTML.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-pandoc -s --metadata title="AVATER help" --include-in-header=help.css --toc --toc-depth=4 -o help_en.html help_en.md
+rm -rf ./build/*
+pandoc -s --metadata title="AVATER help" --include-in-header=help.css --toc --toc-depth=4 -o build/manual_en.html manual_en.md
+cp -v ./*.png build/
