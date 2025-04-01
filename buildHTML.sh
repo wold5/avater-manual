961a14be6013df1178e439736e28777991302752
William Ouwehand <mail@syncoda.nl>
2025-04-01 17:17:13 +0200

    remove pandoc title

--- buildHTML.sh
+++ buildHTML.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm -rf ./build/*
-pandoc -s --metadata title="AVATER help" --include-in-header=help.css --toc --toc-depth=4 -o build/manual_en.html manual_en.md
+pandoc -s --include-in-header=help.css --toc --toc-depth=4 -o build/manual_en.html manual_en.md
 cp -v ./*.png build/
